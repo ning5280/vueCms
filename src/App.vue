75e398f2e50182218e99e6a15283c6939143d705
wangning <529023364@qq.com>
2017-05-08 16:39:37 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+
+  <div id="app">
+    <admin-head></admin-head>
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import adminHead from '@/components/Head.vue'
+export default {
+  name: 'app',
+  components: {
+    adminHead
+  }
+}
+</script>
+
+<style>
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+
+</style>
